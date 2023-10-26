7f0fe02839722fac4692e7ac71a481bb6a0f7bd1
Vitalii Derda <derdavitaliy@gmail.com>
2023-10-26 19:01:19 +0300

    feat: added register page

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -8,16 +8,11 @@ namespace RunnerUp.Controllers;
 
 public class AccountController : Controller
 {
-    private readonly ApplicationDbContext _context;
     private readonly SignInManager<AppUser> _signInManager;
     private readonly UserManager<AppUser> _userManager;
 
-    public AccountController(
-        ApplicationDbContext context, 
-        SignInManager<AppUser> signInManager, 
-        UserManager<AppUser> userManager)
+    public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
     {
-        _context = context;
         _signInManager = signInManager;
         _userManager = userManager;
     }
@@ -40,31 +35,80 @@ public class AccountController : Controller
 
         var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
 
-        if (user != null)
+        if (user == null)
         {
-            // User is found, check password
-            var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
-
-            if (passwordCheck)
-            {
-                // Password is correct, sign in
-                var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
-
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index", "Race");
-                }
-            }
-
-            // Password is incorrect
+            // User not found
             TempData["Error"] = "Wrong credentials. Please, try again";
 
             return View(loginViewModel);
         }
 
-        // User not found
+        // User is found, check password
+        var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+
+        if (passwordCheck)
+        {
+            // Password is correct, sign in
+            var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Race");
+            }
+        }
+
+        // Password is incorrect
         TempData["Error"] = "Wrong credentials. Please, try again";
 
         return View(loginViewModel);
+    }
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        var response = new RegisterViewModel();
+
+        return View(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(registerViewModel);
+        }
+
+        var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+
+        if (user != null)
+        {
+            TempData["Error"] = "This email is already in use";
+
+            return View(registerViewModel);
+        }
+
+        var newUser = new AppUser
+        {
+            Email = registerViewModel.EmailAddress,
+            UserName = registerViewModel.EmailAddress,
+        };
+
+        var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+
+        if (newUserResponse.Succeeded)
+        {
+            await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+        }
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+
+        return RedirectToAction("Index", "Home");
     }
 }
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunnerUp.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "Email address is required")]
+    [Display(Name = "Email Address")]
+    [DataType(DataType.EmailAddress)]
+    public string EmailAddress { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [Display(Name = "Password")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Confirm password is required")]
+    [Display(Name = "Confirm Password")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Passwords do not match")]
+    public string ConfirmPassword { get; set; }
+}
